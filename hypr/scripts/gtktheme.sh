26633c5037763a29ccd4e0abce5cc1e2fd667662
Mathéo Galuba <matheo.galu56@gmail.com>
2025-01-19 12:56:14 +0100

    ✏️ Set more env vars & updated notifications

--- hypr/scripts/gtktheme.sh
+++ hypr/scripts/gtktheme.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+SCHEME='prefer-dark'
 THEME='catppuccin-mocha-mauve-standard+default'
 ICONS='Caticons'
 CURSOR='catppuccin-mocha-dark-cursors'
+SIZE=18
 FONT='FiraCode Nerd Font'
 
 SCHEMA='gsettings set org.gnome.desktop.interface'
 
 apply_themes() {
+  ${SCHEMA} color-scheme "$SCHEME"
   ${SCHEMA} gtk-theme "$THEME"
   ${SCHEMA} icon-theme "$ICONS"
   ${SCHEMA} cursor-theme "$CURSOR"
+  ${SCHEMA} cursor-size "$SIZE"
   ${SCHEMA} font-name "$FONT"
 }
 
